a2841c850c28b8b54214a9feed44c740c34346cd
Leonardo Aguilera Blanco <laguileraab@gmail.com>
2022-11-11 13:34:19 -0500

    first commit

---
+++ src/test/java/com/solintecno/skills/SkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solintecno.skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
